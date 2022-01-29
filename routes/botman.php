6e8286fadd3cd58fd6e4613533d2fe8248259bbe
Vadym Sushynskyi <v.sushinskiy@napopravku.ru>
2022-01-29 15:48:20 +0300

    Start

--- routes/botman.php
+++ routes/botman.php
@@ -6,5 +6,6 @@ use App\Bot\Conversations\MainConversation;
 $botman = resolve('botman');
 
 $botman->hears('/start ([0-9]+)', BotManController::class . '@startConversation');
+$botman->hears('/start', BotManController::class . '@startConversation');
 
 $botman->fallback(fn ($bot) => $bot->startConversation(new MainConversation()));
